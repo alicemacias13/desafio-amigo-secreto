bf04976b3969609dc9eeca718322e491ad013f0b
Alice Macias <alicemacias13@gmail.com>
2025-03-04 18:13:47 -0300

    amigo-secreto

---
+++ app.js
@@ -0,0 +1,42 @@
+let amigos = []
+
+function adicionarAmigo(){  
+    let inputAmigo = document.getElementById("amigo");
+    let nomeAmigo = inputAmigo.value;
+
+    if(!nomeAmigo){
+        alert("Digite o nome do amigo!")
+        return;
+    }
+    amigos.push(nomeAmigo);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    atualizarLista();
+}
+
+function atualizarLista(){
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++){
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert("Adicione um amigo primeiro!");
+        return;
+    }
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = `O amigo sorteado foi: ${sorteado}`;
+
+    let limparLista = document.getElementById("listaAmigos");
+    limparLista.innerHTML = "";
+    amigos = [];
+}
+
+
